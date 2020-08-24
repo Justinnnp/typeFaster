69add96cfc827fccaaa8beddde2c92ee5ce26360
Joris ter Wolbeek <21074092@talnet.nl>
2020-08-24 13:05:37 +0200

    story out database to json converted

--- backend/getStory.php
+++ backend/getStory.php
@@ -2,6 +2,21 @@
     $dsn = "mysql:host=localhost;dbname=typefaster";
     $user = "root";
     $passwd = "";
-
     $pdo = new PDO($dsn, $user, $passwd);
+    
+    startGame($pdo);
+    
+    function startGame($pdo) {
+        $stm = $pdo->query("SELECT * FROM stories");
+        $plannedJson = $stm->fetchAll();
+        storiesToJson($plannedJson);
+    }
+
+    function storiesToJson($actualJson){
+        $storyJson = json_encode($actualJson);
+        echo ($storyJson);
+
+    }
+
+
 ?>
