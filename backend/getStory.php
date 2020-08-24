2db126dd60ba867e28bb10fa8c56802daaa9db8e
Joris ter Wolbeek <21074092@talnet.nl>
2020-08-24 13:10:25 +0200

    added more headers

--- backend/getStory.php
+++ backend/getStory.php
@@ -4,21 +4,20 @@
     $passwd = "";
     $pdo = new PDO($dsn, $user, $passwd);
     header('Content-Type: application/json');
-    
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
     startGame($pdo);
     
     function startGame($pdo) {
         $stm = $pdo->query("SELECT * FROM stories");
         $plannedJson = $stm->fetchAll();
         storiesToJson($plannedJson);
+        
     }
 
     function storiesToJson($actualJson){
         $storyJson = json_encode($actualJson);
         echo ($storyJson);
-        
-
     }
-
-
 ?>
