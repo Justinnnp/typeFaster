7597904ca08c17ec023227798a2478354cef13b5
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-08-24 12:26:17 +0200

    added Typer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Typer from "./Typer.js";
 
 function App() {
     return (
         <div className="App">
             <h1>TypeFaster</h1>
+            <Typer text="This is a sample text."></Typer>
         </div>
     );
 }
