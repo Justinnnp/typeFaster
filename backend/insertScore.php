386b2b6da65402cb594eb6b6065fe77e58a9a7ad
Joris ter Wolbeek <21074092@talnet.nl>
2020-08-24 14:27:21 +0200

    insert highscore into database with name

--- backend/insertScore.php
+++ backend/insertScore.php
@@ -7,5 +7,12 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
 
-    
+   
+
+    gameEnd($pdo);
+
+    function gameEnd($pdo){
+        $stmHighscore = $pdo->prepare("INSERT INTO highscore (wpm, username) VALUES ('".$_POST["wpm"]."' , '".$_POST["username"]."')");
+        $stmHighscore->execute();
+    }
 ?>
